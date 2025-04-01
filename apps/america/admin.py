339fa5f067f1e511a1960b3ba048d64e848d1cba
adylbek0408 <adylbek0408@icloud.com>
2025-04-02 02:34:16 +0600

    update admin add form

--- apps/america/admin.py
+++ apps/america/admin.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
 from .models import America, ComparisonsAmerica
 from apps.common.admin import BaseCarAdmin
+from django import forms
+
+
+class AmericaForm(forms.ModelForm):
+    class Meta:
+        model = America
+        fields = '__all__'
+        widgets = {
+            'url': forms.URLInput(attrs={'required': False}),
+        }
 
 
 @admin.register(America)
 class AmericaAdmin(BaseCarAdmin):
-    pass
+    form = AmericaForm
+
 
 
 @admin.register(ComparisonsAmerica)
